2e8c03ed4b2b2a2d440219380e39e20dc31d9666
Nick <nicksalt24@gmail.com>
2017-11-09 22:06:50 -0500

    - Adjusted File Names/Adjusted Home Screen content location - Added content to home screen - Added selectable highlight to items in nav drawer - Created and added logo assets (see drawable folder) - Increased minimum SDK (15->16) - Added loading splash screen

--- app/src/main/java/ca/nicksalt/appdoc/HomeActivity.java
+++ app/src/main/java/ca/nicksalt/appdoc/HomeActivity.java
@@ -1,16 +1,12 @@
 package ca.nicksalt.appdoc;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity
@@ -19,7 +15,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nav);
+        setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -32,6 +28,8 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //Set Home to current selected in nav bar
+        navigationView.getMenu().getItem(0).setChecked(true);
     }
 
     @Override
@@ -42,6 +40,14 @@ public class HomeActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
     }
 
 
