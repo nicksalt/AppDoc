8f16e31b4c0955db3bf2d9fda6ba9de6e9ba7ee5
Nick <nicksalt24@gmail.com>
2017-11-12 16:56:01 -0500

    Enable Firebase
    - Added Firebase
    - Create Login Activity
    - Update Build Versions, etc.

---
+++ app/src/main/java/ca/nicksalt/appdoc/LoginActivity.java
@@ -0,0 +1,24 @@
+package ca.nicksalt.appdoc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button temp = findViewById(R.id.go_to_home_button);
+        temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/ca/nicksalt/appdoc/SplashActivity.java
+++ app/src/main/java/ca/nicksalt/appdoc/SplashActivity.java
@@ -4,12 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
         // Start home activity
-        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+        if(auth.getCurrentUser() != null) {
+            startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+        } else{
+            //TOBE replaced
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        }
         // close splash activity
         finish();
     }
