af6b91be9aea2546a3059b4533c1e0eba8881896
Nick <nicksalt24@gmail.com>
2017-11-15 04:01:33 -0500

    Made Nav Bar an abstract class

---
+++ app/src/main/java/ca/nicksalt/appdoc/BaseNavDrawerActivity.java
@@ -0,0 +1,85 @@
+package ca.nicksalt.appdoc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by nick on 2017-11-15.
+ */
+
+public abstract class BaseNavDrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    protected FirebaseAuth auth;
+    protected Toolbar toolbar;
+    private static TextView navHeaderEmail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.toolbar_preferences:
+                Toast.makeText(this, "Preferences Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_home) {
+            startActivity(new Intent(BaseNavDrawerActivity.this, HomeActivity.class));
+        } else if (id == R.id.nav_results) {
+            startActivity(new Intent(BaseNavDrawerActivity.this, ResultsActivity.class));
+        } else if (id == R.id.nav_sign_out){
+            auth.signOut();
+            startActivity(new Intent(BaseNavDrawerActivity.this, LoginActivity.class));
+            finish();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
--- app/src/main/java/ca/nicksalt/appdoc/ResultsActivity.java
+++ app/src/main/java/ca/nicksalt/appdoc/ResultsActivity.java
@@ -1,27 +1,21 @@
 package ca.nicksalt.appdoc;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class ResultsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ResultsActivity extends BaseNavDrawerActivity {
 
     private TextView mTextMessage;
 
-    private FirebaseAuth auth;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -48,74 +42,25 @@ public class ResultsActivity extends AppCompatActivity implements NavigationView
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        auth = FirebaseAuth.getInstance();
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         //Set Home to current selected in nav bar
         navigationView.getMenu().getItem(1).setChecked(true);
-    }
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+        View header = navigationView.getHeaderView(0);
+        TextView headerEmail = header.findViewById(R.id.nav_email);
+        try {
+            headerEmail.setText(auth.getCurrentUser().getEmail());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            headerEmail.setText("");
         }
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.toolbar_preferences:
-                Toast.makeText(this, "Preferences Clicked", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        return true;
-
-    }
-
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            startActivity(new Intent(ResultsActivity.this, HomeActivity.class));
-        } else if (id == R.id.nav_sign_out){
-            auth.signOut();
-            startActivity(new Intent(ResultsActivity.this, LoginActivity.class));
-            finish();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_toolbar, menu);
-        return true;
     }
 }
