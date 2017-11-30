4e1ae640dc683ca1e327930d15b484f1561c79dc
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2017-11-30 17:47:19 -0500

    Made a Preference Activity

---
+++ app/src/main/java/ca/nicksalt/appdoc/SettingsActivity.java
@@ -0,0 +1,34 @@
+package ca.nicksalt.appdoc;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new PreferencesActivity()).commit();
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+
+
+}
+
